eca67d350cf53bf016984cd0e89549d3cc95539c
Thaís <88781888+op3ny@users.noreply.github.com>
2026-08-27 20:24:38 -0300

    Delete hps/HPS-BROWSER directory

--- hps/HPS-BROWSER/browser-cs/Models/ExchangeIssuerSummary.cs
+++
@@ -1,8 +0,0 @@
-namespace HpsBrowser.Models;
-
-public sealed class ExchangeIssuerSummary
-{
-    public string Issuer { get; set; } = string.Empty;
-    public int Count { get; set; }
-    public int Total { get; set; }
-}
